514b1bad273b45c547b5289db2bc24475702d7c9
YohanSamitha <yohansamitha123@gmail.com>
2021-07-28 16:00:39 +0530

    update

--- src/app/modules/auth/components/login-page/inner-items/login-items-page/login-items-page.component.ts
+++ src/app/modules/auth/components/login-page/inner-items/login-items-page/login-items-page.component.ts
@@ -24,8 +24,6 @@ export class LoginItemsPageComponent implements OnInit {
   }
 
   //handling login function
-  //handling login function
-  //handling login function
   login() {
     this.userService.login(
       this.loginForm.get('email')?.value.toString().trim(),
